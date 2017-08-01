843732f77fc4d60f9f2777e57ea4d50e4c35b83b
HuangXiao <hx36w35@163.com>
2017-08-01 17:36:34 +0800

    SingleFragmentActivity抽象类添加

---
+++ HomeMail/app/src/main/java/cn/edu/nju/homemail/SingleFragmentActivity.java
@@ -0,0 +1,35 @@
+package cn.edu.nju.homemail;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by huangxiao on 2017/8/1.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+    }
+
+}
